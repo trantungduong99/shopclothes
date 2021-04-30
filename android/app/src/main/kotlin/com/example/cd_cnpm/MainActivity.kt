997b2ab61eebe6a6abf54ad77e2ccb5d5ead0b0d
unknown <trantungduong99@gmail.com>
2021-04-30 16:15:22 +0700

    change_shop

--- android/app/src/main/kotlin/com/example/cd_cnpm/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cd_cnpm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_app/src/config/route.dart';
-import 'package:flutter_ecommerce_app/src/pages/mainPage.dart';
-import 'package:flutter_ecommerce_app/src/pages/loginPage.dart';
-import 'package:flutter_ecommerce_app/src/pages/product_detail.dart';
-import 'package:flutter_ecommerce_app/src/widgets/customRoute.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'src/themes/theme.dart';
-import 'dart:developer' as developer;
+import 'package:flutter_watch_shop/app.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E-Commerce ',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      routes: Routes.getRoute(),
-      onGenerateRoute: (RouteSettings settings) {
-        developer.log(settings.name);
-        if (settings.name.contains('detail')) {
-          return CustomRoute<bool>(
-              builder: (BuildContext context) => ProductDetailPage());
-        }
-        if (settings.name.contains('Login')) {
-          return CustomRoute<bool>(
-              builder: (BuildContext context) => LoginPage());
-        }
-        else {
-          return CustomRoute<bool>(
-              builder: (BuildContext context) => MainPage());
-        }
-      },
-      initialRoute: "LoginPage",
-    );
-  }
-}
